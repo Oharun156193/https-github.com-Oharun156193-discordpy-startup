dec3623a58311778a5e3f624b7b6b0953cbe9485
Oharun156193 <89328327+Oharun156193@users.noreply.github.com>
2021-08-22 13:13:17 +0900

    stopwatch1

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,39 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+    
+import time
 
+
+def convert(sec):
+    minits = sec // 60
+    second = sec % 60
+    milli_sec = (second - int(second)) * 1000
+    hour = minits // 60
+    min = minits % 60
+    return f"{int(hour)}:{int(min)}:{int(second)}:{int(milli_sec)}"
+
+
+@client.event
+async def on_message(message):
+    # メッセージ送信者がmemberだった場合は無視する
+    if message.author.member:
+        return
+    # 「/start」と発言したら「にゃーん」が返る処理
+    if message.content == '/start':
+    #/startとbotが発言したときタイマーが始まる
+    start_signal = input('Push "ENTER" to Start')
+    start_time = time.time()
+    stop_signal = input('Push "ENTER" to Stop')
+    stop_time = time.time()
+    result = stop_time - start_time
+    print(f"Stop Time：{result:.3f} sec")
+    
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    
+    
+    
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
